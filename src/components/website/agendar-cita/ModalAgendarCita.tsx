117de0b9428a9ccfcdd1afec850d3a6c4bbcb34d
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-11-09 10:21:31 -0600

    [UPDATE] Streamline routing logic to consistently push to homepage in ModalAgendarCita component on modal close action

--- src/components/website/agendar-cita/ModalAgendarCita.tsx
+++ src/components/website/agendar-cita/ModalAgendarCita.tsx
@@ -171,7 +171,7 @@ export function ModalAgendarCita() {
         if (isOpen) {
           route.push("/");
         } else {
-          route.back();
+          route.push("/");
         }
       }}
     >
